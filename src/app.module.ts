28cef02868c32eec79bdf13906dd261ae7a42ca1
Tim Garthwaite <tim@hydrow.com>
2020-11-25 14:11:00 -0500

    Stay DRY, my friends.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,35 +1,14 @@
-import * as path from "path";
-
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { dbOptions } from './database-config';
 import { Construed } from './db/construed.entity';
-
-function envOrFail(key: string): string {
-    const value = process.env[key];
-    if (!value) {
-        throw new Error(`process.env.${key} must be set.`);
-    }
-    return value;
-}
 
 @Module({
     imports: [
-        TypeOrmModule.forRoot({
-            database: envOrFail('DB_NAME'),
-            entities: [path.resolve(__dirname, '**', 'db', '**', '*.entity.{ts,js}')],
-            host: process.env.DB_HOST || 'localhost',
-            migrations: [path.resolve(__dirname, '**', 'db', 'migrations', '*.{ts,js}')],
-            namingStrategy: new SnakeNamingStrategy(),
-            password: envOrFail('DB_PASSWORD'),
-            port: Number(process.env.DB_PORT) || 5432,
-            synchronize: true,
-            type: 'postgres',
-            username: envOrFail('DB_USERNAME'),
-        }),
+        TypeOrmModule.forRoot(dbOptions),
         TypeOrmModule.forFeature([
             Construed,
         ]),
